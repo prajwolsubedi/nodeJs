856d0f3feed9758bc76685b164816830ce77d01d
prajwol subedi <prajwolsubedizzz@gmail.com>
2024-05-27 16:25:40 +0545

    update

--- 3-express/app.js
+++ 3-express/app.js
@@ -1,123 +1,46 @@
-const fs = require('fs');
 const express = require('express');
-const morgan = require("morgan");
-
+const morgan = require('morgan');
 const app = express();
 
+//---------1) MIDDLEWARE -------
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
+// app.use((req, res, next) => {
+//   console.log("hello from the middleware 👋");
+//   next();
+// })
 
-//1) MIDDLEWARE
-
-app.use(morgan("dev"));
-
-// app.get('/', (req, res) => {
-//   res.status(200).send('Hello from the server!');
-// });
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-
-const getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    result: tours.length,
-    data: {
-      tours,
-    },
-  });
-};
-
-const getTourById = (req, res) => {
-  console.log(req.params);
-  const id = req.params.id * 1;
-
-  const tour = tours.find((el) => el.id === id);
-
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid Id',
-    });
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
-};
-
-const createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign(
-    {
-      id: newId,
-    },
-    req.body
-  );
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-};
-
-const updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid Id',
-    });
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated Tour here....>',
-    },
-  });
-};
-
-const deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    res.status(400).json({
-      status: 'fail',
-      message: 'Invalid id',
-    });
-  }
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
-};
-
-// app.get('/api/v1/tours', getAllTours);
-// app.post('/api/v1/tours', createTour);
-
-// app.get('/api/v1/tours/:id', getTourById);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
-
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
-
-app
-  .route('/api/v1/tours/:id')
-  .get(getTourById)
-  .patch(updateTour)
-  .delete(deleteTour);
-
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
 });
+
+//-----ROUTES-----
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  console.log('Inside the route not found -------💥💥');
+  next(new AppError(`Can't find ${req.originalUrl} on the server`, 404));
+  // const err = new Error(`Can't find ${req. originalUrl} on the server`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+  // next(err);
+  //If we any argument in next it will automatically assume ther is error and skip all the middleware stack to reach the global error handling middleware
+});
+
+//ERROR HANDLING MIDDLEWARE
+//by specifying 4 params express already knows this is error handling middleware
+app.use(globalErrorHandler);
+
+module.exports = app;
