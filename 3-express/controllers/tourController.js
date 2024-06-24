467c7b0f33b9c4a663891ae1c53926c32a54f62e
prajwol subedi <prajwolsubedizzz@gmail.com>
2024-06-24 23:04:30 +0545

    update

--- 3-express/controllers/tourController.js
+++ 3-express/controllers/tourController.js
@@ -142,7 +142,7 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 exports.getTourById = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
   if (!tour) {
-    return next(new AppError(`No tour found with that ID`, 404));
+    return next(new AppError(`No tour found with that Id`, 404));
   }
   res.status(200).json({
     status: 'success',
